f8be9dc22dc2a897e143458f673ed462e4f6e3ab
Albert Gil Escura <>
2022-09-25 10:24:04 +0200

    Create package

---
+++ Sources/snapshot-testing-for-composable-architecture/snapshot_testing_for_composable_architecture.swift
@@ -0,0 +1,6 @@
+public struct snapshot_testing_for_composable_architecture {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/snapshot-testing-for-composable-architectureTests/snapshot_testing_for_composable_architectureTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import snapshot_testing_for_composable_architecture
+
+final class snapshot_testing_for_composable_architectureTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(snapshot_testing_for_composable_architecture().text, "Hello, World!")
+    }
+}
